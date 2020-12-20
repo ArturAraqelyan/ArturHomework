39074bf812938fb29d802bd6fc320474ec6bcbfc
ArturAraqelyan <098050446a>
2020-12-20 13:10:49 +0300

    LessonOneExOne

--- java/LessonOneExOne.java
+++ java/LessonOneExOne.java
@@ -1,5 +1,10 @@
 public class LessonOneExOne {
     public static void main(String[] args) {
-
+        int a = 5;
+        if (a % 2 == 1) {
+            System.out.println("не четное");
+        } else {
+            System.out.println("четное");
+        }
     }
 }
