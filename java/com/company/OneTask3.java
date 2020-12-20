225b540eef80a05db0a5aeb797dcd239ddeca2a1
ArturAraqelyan <098050446a>
2020-12-20 12:34:08 +0300

    hell

---
+++ java/com/company/OneTask3.java
@@ -0,0 +1,21 @@
+package com.company;
+/*3.Пользователю необходимо ввести число с клавиатуры.
+Если оно является положительным, то прибавить к нему 2;
+в противном случае умножить на 2.
+Вывести полученное число.
+*/
+import java.util.Scanner;
+
+public class OneTask3 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int a = 3;
+        if (a > 0){
+            a = a + 2;}
+        else {
+            a = a * 2;}
+
+            System.out.println("a");
+        }
+    }
