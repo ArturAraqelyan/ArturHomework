9e8bb1fbb6e063b46f0c49f957c8bb8e27907698
ArturAraqelyan <098050446a>
2020-12-20 14:15:46 +0300

    LessonOneExFour

---
+++ java/LessonOneExFour.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class LessonOneExFour {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int a = 3;
+        if (a > 0) {
+
+            a = a + 1;
+        }
+
+        if (a < 0) {
+            a = a - 2;
+        } else {
+            a = 10;
+
+            System.out.println(a);
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
