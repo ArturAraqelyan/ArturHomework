3dd1709e8941d0d155460bf9dabd0b24348414c3
ArturAraqelyan <098050446a>
2020-12-20 13:53:16 +0300

    LessonOneExThree

---
+++ java/LessonOneExThree.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class LessonOneExThree {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("a:");
+        int a = in.nextInt();
+        if (a > 0) a++;
+        System.out.print("a");
+    }
+}
