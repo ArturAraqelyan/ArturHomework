485bdd6064f41901f692bae29e721a05175de147
ArturAraqelyan <098050446a>
2020-12-20 11:55:35 +0300

    hell

--- java/com/company/OneTask.java
+++ java/com/company/OneTask.java
@@ -1,17 +1,21 @@
- package com.company;
-/*1.Пользователю необходимо ввести число с клавиатуры
+package com.company;
+
+/*2.Пользователю необходимо ввести число с клавиатуры
 Если оно является положительным, то прибавить к нему 1;
 в противном случае не изменять его. Вывести полученное число.*/
 import java.util.Scanner;
 
 public class OneTask {
-    public static void main(String[] args){
 
+    public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
         System.out.print("a:");
         int a = in.nextInt();
         if (a > 0) a++;
         System.out.print("a");
+
+
+
     }
 }
---
+++ java/com/company/OneTask2.java
@@ -0,0 +1,15 @@
+package com.company;
+/* 2.Необходимо написать программу для определения не четного числа.
+Пользователю необходимо ввести число с клавиатуры а программа сообщит
+Число четное или не четное.*/
+
+  public class OneTask2 {
+      public static void main(String[] args) {
+          int a = 5, b = 3;
+          if (a % 2 == 1) {
+              System.out.println("ne chetnogo");
+          } else {
+              System.out.println("chetnogo");
+          }
+      }
+  }
