0e3fd8df0afebde15476f069b37df477dc1373d9
ArturAraqelyan <098050446a>
2020-12-20 13:24:16 +0300

    commit

--- java/com/company/OneTask3.java
+++ java/com/company/OneTask3.java
@@ -4,18 +4,20 @@ package com.company;
 в противном случае умножить на 2.
 Вывести полученное число.
 */
+
 import java.util.Scanner;
 
 public class OneTask3 {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-        int a = 3;
-        if (a > 0){
-            a = a + 2;}
-        else {
-            a = a * 2;}
-
-            System.out.println("a");
+        int a = in.nextInt();
+        if (a > 0) {
+            a = a + 2;
+        } else {
+            a = a * 2;
         }
+
+        System.out.println(a);
     }
+}
