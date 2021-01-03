670bac34463274ad143fced48331afe3b579df22
ArturAraqelyan <098050446a>
2021-01-03 13:55:50 +0400

    LessonTwoExFour

---
+++ java/LessonTwoExFour.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class LessonTwoExFour {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("a");
+        int a = in.nextInt();
+        System.out.print("b");
+        int b = in.nextInt();
+
+        int sum = 0;
+        for (int i = a; i <= b; i++) ;
+
+        System.out.println(sum);
+    }
+}
