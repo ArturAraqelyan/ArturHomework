90010be98bed060ee7d94e420ee2469d6bb51aaa
ArturAraqelyan <098050446a>
2021-01-03 13:08:38 +0400

    LessonOneExFive

---
+++ java/LessonOneExFive.java
@@ -0,0 +1,17 @@
+public class LessonOneExFive {
+    public  static void main(String[] args){
+        int a=5;
+        if(a>0){
+            a=a*2;
+        }
+        else if (a<0){
+            a=a*3;
+        }
+        else {
+            a=10;
+        }
+        System.out.println("a");
+    }
+
+
+}
