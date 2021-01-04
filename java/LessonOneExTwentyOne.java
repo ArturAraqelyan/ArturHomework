3ee5add6fc7f5254da1ad8750087c81a4ee0236a
ArturAraqelyan <098050446a>
2021-01-04 14:46:10 +0400

    LessonOneExTwentyOne

---
+++ java/LessonOneExTwentyOne.java
@@ -0,0 +1,10 @@
+public class LessonOneExTwentyOne {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 10;
+        int c = 5;
+        if (a > b & a > c) {
+            System.out.println("a  mece b ic yev mece c ic");
+        }
+    }
+}
