dedc8795b61ea7c86e8bec88ea0f4523bb98dd30
ArturAraqelyan <098050446a>
2021-01-04 19:46:27 +0400

    LessonTwentyTwo

---
+++ java/LessonTwentyTwo.java
@@ -0,0 +1,11 @@
+public class LessonTwentyTwo {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 12;
+        int c = 14;
+        if (a < b & a < c) {
+            System.out.println("a меньше b и меньше c");
+
+        }
+    }
+}
