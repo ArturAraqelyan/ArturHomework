5b9c46b1116b07326b336cb5d89254a8dccde02e
ArturAraqelyan <098050446a>
2021-01-04 19:31:47 +0400

    LessonTwentyOne

---
+++ java/LessonTwentyOne.java
@@ -0,0 +1,11 @@
+public class LessonTwentyOne {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 10;
+        int c = 5;
+        if (a > b & a > c) {
+            System.out.println("a больше b и больше c");
+        }
+    }
+
+}
