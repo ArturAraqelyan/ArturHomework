48f1683c28315e994d421dc7b07cbedb9a8b9d26
ArturAraqelyan <098050446a>
2021-01-05 16:00:30 +0400

    LessonTwentyThree

---
+++ java/LessonTwentyThree.java
@@ -0,0 +1,11 @@
+public class LessonTwentyThree {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 6;
+        int c = 5;
+        boolean d = a == a;
+        boolean e = a == b;
+        System.out.println(a == a);
+        System.out.println(a == b);
+    }
+}
