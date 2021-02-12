5f1f406d77341fa771afb937b67d14bba8a3d09c
ArturAraqelyan <098050446a>
2021-02-12 13:29:13 +0400

    LessonExTsevenThree

--- java/LessonExTsevenThree.java
+++ java/LessonExTsevenThree.java
@@ -6,10 +6,10 @@ public class LessonExTsevenThree {
         int beginLength = abc.length();
         String s = "ThisIsALongLongWord";
         for (char chStr : s.toLowerCase().toCharArray()) {
-            abc = abc.replace(chStr, '-');
+            abc = abc.replace(chStr, '=');
 
         }
-        abc = abc.replaceAll("hsdgpwqide", "lmbvcxzsdfrt");
-        System.out.printf("Строка содержит 12 разных букв", (beginLength - abc.length()));
+        abc = abc.replaceAll("=", "");
+        System.out.printf("Строка содержит \'%d\' разных букв", (beginLength - abc.length()));
     }
 }
