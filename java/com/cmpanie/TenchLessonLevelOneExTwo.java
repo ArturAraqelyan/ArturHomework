dbf083cf14099b42c1e0bceb49271c2149c6e48a
ArturAraqelyan <098050446a>
2021-02-16 22:25:47 +0400

    TenchLessonLevelOneExTwo

---
+++ java/com/cmpanie/TenchLessonLevelOneExTwo.java
@@ -0,0 +1,12 @@
+package com.cmpanie;
+
+public class TenchLessonLevelOneExTwo {
+    public static void main(String[] args) {
+        int[] sum = new  int[] {2,4,6,8};
+        for (int i=0; i<sum.length;i++){
+            sum[i]=sum[i] *2;
+            System.out.println(sum[i]);
+        }
+
+    }
+}
